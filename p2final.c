7eb1489cac17495839c393f00cf089b2324d4806
BalajiVirat114 <94665564+BalajiVirat114@users.noreply.github.com>
2021-12-04 19:07:00 +0530

    no changes

---
+++ p2final.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("Enter two numbers");
+  scanf("%d",&a);
+  return a;
+}
+int add(int a,int b)
+{
+  int sum;
+  sum=a+b;
+  return sum;
+}
+int output(int a, int b,int sum)
+{
+  printf("%d + %d is %d\n",a,b,sum);
+  return sum;
+}
+int main()
+{
+  int x,y,z;
+  x=input();
+  y=input();
+  z=add(x,y);
+  output(x,y,z);
+  return 0;
+  
+}
+
